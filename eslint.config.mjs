afe35d2c1e048a30dca1fe3261432f22c4a0d4e6
rdrigos <rafaeldrigo.rds@gmail.com>
2025-08-01 22:50:01 -0300

    feat: Init

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { ignores: ['dist/**', 'node_modules/**'] },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  }
+);
